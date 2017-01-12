6accb180d8acd7513d8180e1d037b5328a3cf8ac
Elliott Stoneham <elliott.stoneham@gmail.com>
2017-01-12 14:44:08 +0000

    demonstrate go vendoring using app engine SDK

---
+++ go/vendortest/vendortest/vendortest.go
@@ -0,0 +1,7 @@
+// for why this crazy arrangement is required, see http://stackoverflow.com/questions/39948027/how-do-i-make-vendoring-work-with-google-app-engine
+
+package vendortest
+
+import _ "github.com/appscale/sample-apps/go/vendortestcode/vendortestcode"
+
+func init() {}
