9c098d27503d590268f51e82492000b008d2e236
Graziano Obertelli <graziano@appscale.com>
2017-09-07 16:15:53 -0700

    Added faststart guestbook, and wait handler for it

--- python2.7/guestbook/guestbook.py
+++ python2.7/guestbook/guestbook.py
@@ -1,75 +1,65 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-import cgi
-import datetime
+import jinja2
+import os
 import webapp2
 
-from google.appengine.ext import ndb
 from google.appengine.api import users
+from google.appengine.ext import ndb
 
-guestbook_key = ndb.Key('Guestbook', 'default_guestbook')
+
+# We set a parent key on the 'Greetings' to ensure that they are all in the same
+# entity group. Queries across the single entity group will be consistent.
+# However, the write rate should be limited to ~1/second.
+
+def guestbook_key(guestbook_name='default_guestbook'):
+    return ndb.Key('Guestbook', guestbook_name)
+
+jinja_environment = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
 
 class Greeting(ndb.Model):
-  author = ndb.UserProperty()
-  content = ndb.TextProperty()
-  date = ndb.DateTimeProperty(auto_now_add=True)
-
+    author = ndb.UserProperty()
+    content = ndb.StringProperty(indexed=False)
+    date = ndb.DateTimeProperty(auto_now_add=True)
 
 class MainPage(webapp2.RequestHandler):
-  def get(self):
-    self.response.out.write('<html><body>')
+    def get(self):
+        greetings_query = Greeting.query(ancestor=guestbook_key()).order(-Greeting.date)
+        greetings = greetings_query.fetch(10)
 
-    greetings = ndb.gql('SELECT * '
-                        'FROM Greeting '
-                        'WHERE ANCESTOR IS :1 '
-                        'ORDER BY date DESC LIMIT 10',
-                        guestbook_key)
+        if users.get_current_user():
+            url = users.create_logout_url(self.request.uri)
+            url_linktext = 'Logout'
+        else:
+            url = users.create_login_url(self.request.uri)
+            url_linktext = 'Login'
 
-    for greeting in greetings:
-      if greeting.author:
-        self.response.out.write('<b>%s</b> wrote:' % greeting.author.nickname())
-      else:
-        self.response.out.write('An anonymous person wrote:')
-      self.response.out.write('<blockquote>%s</blockquote>' %
-                              cgi.escape(greeting.content))
+        template = jinja_environment.get_template('index.html')
+        self.response.out.write(template.render(greetings=greetings,
+                                                url=url,
+                                                url_linktext=url_linktext))
 
 
-    self.response.out.write("""
-          <form action="/sign" method="post">
-            <div><textarea name="content" rows="3" cols="60"></textarea></div>
-            <div><input type="submit" value="Sign Guestbook"></div>
-          </form>
-        </body>
-      </html>""")
+class WaitHandler(webapp2.RequestHandler):
+    def get(self):
+        time.sleep(30)
+        self.response.out.write('done')
 
 
 class Guestbook(webapp2.RequestHandler):
-  def post(self):
-    greeting = Greeting(parent=guestbook_key)
+    def post(self):
+        greeting = Greeting(parent=guestbook_key())
 
-    if users.get_current_user():
-      greeting.author = users.get_current_user()
+        if users.get_current_user():
+            greeting.author = users.get_current_user()
 
-    greeting.content = self.request.get('content')
-    greeting.put()
-    self.redirect('/')
+        greeting.content = self.request.get('content')
+        greeting.put()
+        self.redirect('/')
 
-
-app = webapp2.WSGIApplication([
-  ('/', MainPage),
-  ('/sign', Guestbook)
+application = webapp2.WSGIApplication([
+    ('/', MainPage),
+    ('/wait', WaitHandler),
+    ('/sign', Guestbook),
 ], debug=True)
